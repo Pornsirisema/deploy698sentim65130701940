b5c46fe83fcdf8317aa2f7b365f3281c7ddd03f5
Pornsirisema <pornsiri.se@gmail.com>
2024-11-03 18:45:08 +0700

    Update app.sa.nb.py

--- app.sa.nb.py
+++ app.sa.nb.py
@@ -6,19 +6,20 @@ import streamlit as st
 
 # Load the saved model
 with open('sentiment_pipeline_model.pkl', 'rb') as file:
-      loaded_model = pickle. load(file)
+    loaded_model = pickle.load(file)
 
 # Streamlit app
-      st.title("Sentiment Analysis")
+st.title("Sentiment Analysis")
 
 # Input box for user to type a sentence
 user_input = st.text_input("Enter a sentence:")
 
 # Predict sentiment when user submits input
-if st. button("Predict Sentiment"):
-  if user_input:
-      prediction = loaded_model.predict([user_input])
-      sentiment = "Positive" if prediction [0] == 1 else "Negative"
-      st.write(f"Predicted Sentiment: {prediction[0]}")
-else:
-      st.write("Please enter a sentence to analyze.")
+if st.button("Predict Sentiment"):
+    if user_input:
+        prediction = loaded_model.predict([user_input])
+        sentiment = "Positive" if prediction[0] == 1 else "Negative"
+        st.write(f"Predicted Sentiment: {sentiment}")
+    else:
+        st.write("Please enter a sentence to analyze.")
+
